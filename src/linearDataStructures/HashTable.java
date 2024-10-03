180d42f940f4540d2978e22a42e52e82dc16c1b1
petia duskin <petr.dyuskin@mail.ru>
2024-10-03 17:31:02 +0500

    refactor method for finding first non repeated character

--- src/linearDataStructures/HashTable.java
+++ src/linearDataStructures/HashTable.java
@@ -6,15 +6,12 @@ import java.util.HashMap;
 public class HashTable {
     public static void firstNonRepeatedCharacter(String str) {
         HashMap<Character, Integer> map = new HashMap<>();
-        java.util.Queue<Character> queue = new ArrayDeque<>();
 
         for (char ch : str.toCharArray()) {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
-            queue.add(ch);
         }
 
-        while (!queue.isEmpty()) {
-            char ch = queue.remove();
+       for (char ch : str.toCharArray()) {
             if (map.get(ch) == 1) {
                 System.out.println(ch);
                 return;
