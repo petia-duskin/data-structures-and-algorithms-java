3a9b83a04fc8bcd1d54150bb295bed69ba5a8dca
petia duskin <petr.dyuskin@mail.ru>
2024-10-26 21:11:11 +0500

    add reverse polish notation

---
+++ src/generalAlgorithms/Main.java
@@ -0,0 +1,8 @@
+package generalAlgorithms;
+
+public class Main {
+    public static void main(String[] args) {
+        ReversePolishNotation reversePolishNotation = new ReversePolishNotation();
+        reversePolishNotation.reversePolishNotation("1 + 3 * 1 - 1");
+    }
+}
