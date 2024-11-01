2629f5e0d93dfac27370491c80bff8fa5fab11fe
petia duskin <petr.dyuskin@mail.ru>
2024-11-01 22:58:09 +0500

    implement part of remove method

--- src/treeDataStructures/BTree.java
+++ src/treeDataStructures/BTree.java
@@ -1,6 +1,8 @@
 package treeDataStructures;
 
 
+import java.util.Arrays;
+
 // Preemptive node split
 class BTree {
     private static final int T = 2; // Минимальная степень дерева (макс. 2*T - 1 элементов в узле)
@@ -123,6 +125,37 @@ class BTree {
                 rightChild.children[j] = node.children[j + T];
             }
         }
+
+        private boolean hasLeftChild(int index) {
+            return index > 0 && getChild(index - 1) != null;
+        }
+
+        private boolean hasRightChild(int index) {
+            return index < children.length && getChild(index + 1) != null;
+        }
+
+        private Node getLeftChild(int index) {
+            return getChild(index - 1);
+        }
+
+        private Node getRightChild(int index) {
+            return getChild(index + 1);
+        }
+
+        private int getPredecessor(int childIndex) {
+            Node predecessor = getChild(childIndex - 1);
+            return predecessor.getValue(predecessor.valuesCount - 1);
+        }
+
+        private int getSuccessor(int childIndex) {
+            Node successor = getChild(childIndex + 1);
+            return successor.getValue(0);
+        }
+
+        @Override
+        public String toString() {
+            return Arrays.toString(Arrays.copyOfRange(values, 0, valuesCount));
+        }
     }
 
     private boolean hasMaxSize(Node node) {
@@ -178,6 +211,147 @@ class BTree {
         print(root, "");
     }
 
+    // Сперва нам надо найти parent узла, который мы будем удалять
+    public void remove(int value) {
+        Node parentNode = getParent(value);
+        if (parentNode == null) {
+            throw new IllegalArgumentException();
+        }
+
+        int childIndex = searchChildIndex(parentNode, value);
+        Node removeNode = parentNode.getChild(childIndex);
+
+        removeValue(removeNode, value);
+
+        System.out.println("parent " + parentNode);
+
+        if (isValidSize(removeNode)) {
+            System.out.println(removeNode + " size is valid");
+        } else {
+            System.out.println(removeNode + " size is not valid");
+            if (parentNode.hasLeftChild(childIndex)) {
+                System.out.println(removeNode + " has left child");
+                if (mayBorrow(parentNode.getLeftChild(childIndex))) {
+                    int predecessor = parentNode.getPredecessor(childIndex);
+                    Node predecessorNode = parentNode.getLeftChild(childIndex);
+                    System.out.println("We can borrow " + parentNode.getPredecessor(childIndex));
+                    removeNode.values[0] = parentNode.values[0];
+                    parentNode.values[0] = predecessor;
+                    removeNode.valuesCount = removeNode.valuesCount + 1;
+                    predecessorNode.valuesCount = predecessorNode.valuesCount - 1;
+                } else {
+                    System.out.println("We can't borrow " + parentNode.getPredecessor(childIndex));
+                }
+            } else if (parentNode.hasRightChild(childIndex)) {
+                System.out.println(removeNode + " has right child");
+                if (mayBorrow(parentNode.getRightChild(childIndex))) {
+                    int successor = parentNode.getSuccessor(childIndex);
+                    Node successorNode = parentNode.getRightChild(childIndex);
+                    System.out.println("We can borrow " + parentNode.getSuccessor(childIndex));
+                    int parentValue = parentNode.values[parentNode.valuesCount - 1];
+                    removeNode.values[0] = parentValue;
+                    parentNode.values[parentNode.valuesCount - 1] = successor;
+                    removeNode.valuesCount = removeNode.valuesCount + 1;
+                    shiftItemsToRemove(0, successorNode.valuesCount, successorNode.values);
+                    successorNode.valuesCount = successorNode.valuesCount - 1;
+                } else {
+                    System.out.println("We can't borrow " + parentNode.getSuccessor(childIndex));
+                }
+            } else {
+                // merge the node with either the left sibling node or the right sibling node
+                System.out.println("We can't borrow from left and right");
+                if (parentNode.hasLeftChild(childIndex)) {
+                    Node predecessorNode = parentNode.getLeftChild(childIndex - 1);
+                    int[] leftValues = Arrays.copyOfRange(predecessorNode.values, 0, predecessorNode.valuesCount);
+                    shiftItemsToRemove(0, leftValues.length, removeNode.values);
+                    for (int i = 0; i < leftValues.length; i++) {
+                        removeNode.values[i] = leftValues[i];
+                    }
+
+                    parentNode.getChild(childIndex - 1).valuesCount = 0;
+                    removeNode.valuesCount = removeNode.valuesCount + leftValues.length;
+                } else if (parentNode.hasRightChild(childIndex)) {
+                    Node successorNode = parentNode.getRightChild(childIndex + 1);
+                    int[] rightValues = Arrays.copyOfRange(successorNode.values, 0, successorNode.valuesCount);
+                    int index = removeNode.valuesCount;
+                    int successorIndex = 0;
+                    while (successorIndex < rightValues.length) {
+                        removeNode.values[index++] = rightValues[successorIndex++];
+                    }
+
+                    parentNode.getRightChild(childIndex - 1).valuesCount = 0;
+                    removeNode.valuesCount = removeNode.valuesCount + rightValues.length;
+                }
+            }
+        }
+
+
+    }
+
+    private void removeValue(Node removeNode, int value) {
+        for (int i = 0; i < removeNode.valuesCount; i++) {
+            if (removeNode.getValue(i) == value) {
+                shiftItemsToRemove(i, removeNode.valuesCount - 1, removeNode.values);
+                removeNode.valuesCount = removeNode.valuesCount - 1;
+                return;
+            }
+        }
+    }
+
+    private boolean isValidSize(Node node) {
+        return node.valuesCount >= MIN_NODE_SIZE;
+    }
+
+    private boolean mayBorrow(Node node) {
+        return node.valuesCount > MIN_NODE_SIZE;
+    }
+
+    private void shiftItemsToRemove(int from, int to, int[] arr) {
+        for (int i = from; i < to; i++) {
+            arr[i] = arr[i + 1];
+        }
+    }
+
+    private Node getParent(int value) {
+        return getParent(root, root, value);
+    }
+
+    private int searchChildIndex(Node node, int value) {
+        return searchChildIndex(node, 0, value);
+    }
+
+    private int searchChildIndex(Node node, int prevIndex, int value) {
+        int currentIndex = 0;
+
+        while (currentIndex < node.valuesCount && value > node.getValue(currentIndex)) {
+            currentIndex++;
+        }
+
+        if (node.getValue(currentIndex) == value) {
+            return prevIndex;
+        } else if (node.isLeaf) {
+            return -1;
+        }
+
+        return searchChildIndex(node.getChild(currentIndex), currentIndex, value);
+    }
+
+    private Node getParent(Node node, Node prev, int value) {
+        int index = 0;
+
+        while (index < node.valuesCount && value > node.getValue(index)) {
+            index++;
+        }
+
+        if (node.getValue(index) == value) {
+            return prev;
+        } else if (node.isLeaf) {
+            return null;
+        }
+
+        return getParent(node.getChild(index), node, value);
+    }
+
     private void print(Node node, String indent) {
         System.out.print(indent);
         for (int i = 0; i < node.valuesCount; i++) {
@@ -195,18 +369,23 @@ class BTree {
     public static void main(String[] args) {
         BTree tree = new BTree();
 
-        tree.insert(10);
         tree.insert(20);
-        tree.insert(30);
-        tree.insert(40); // root split
-        tree.insert(15);
-        tree.insert(41);
-        tree.insert(1);
-        tree.insert(35);
+        tree.insert(40);
+        tree.insert(10);
+        tree.insert(28);
+        tree.insert(33);
+        tree.insert(50);
+        tree.insert(60);
         tree.insert(5);
-        tree.insert(6);
-        tree.insert(7);
+        tree.insert(15);
+        tree.insert(25);
+        tree.insert(30);
+        tree.insert(35);
+        tree.insert(45);
+        tree.insert(55);
+        tree.insert(65);
 
-        System.out.println(tree.search(55));
+        tree.remove(30);
+        System.out.println("Debug message");
     }
 }
